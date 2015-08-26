55c964327b4669371fb800d71e237e6762058e9c
rafaelasouza <rafaela.azevedosouza@gmail.com>
2015-08-26 21:46:18 +0100

    Added OpenApplication class

---
+++ OpenApplication.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+
+namespace ProjectName
+{
+	public class Application{
+
+	        public static void Open()
+	        {
+	        		System.Diagnostics.Process proc = new System.Diagnostics.Process();
+	            	proc.EnableRaisingEvents = false;
+	            	proc.StartInfo.FileName = "C:\\Users\\yourUser\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\UI.appref-ms";
+	        	 try
+	            	{ 				
+	            		proc.Start();
+						WaitApplicationLoad();
+					}
+ 				catch (Exception e)
+	             	{
+	                   	MessageBox.Show(e.Message);
+	               	}
+	        }
+
+	        public static bool WaitApplicationLoad(){
+	        	WinButton button = new WinButton();
+				while(!button.WaitForControlExist())
+					{
+						PlayBack.Wait(5000);
+					}
+	        }	
+	}
+}
